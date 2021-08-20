73f0d2b4f0df5cfb4045f39c20508dc34d26c521
Ignas Razmys <49831801+ignasTheRobotGuy@users.noreply.github.com>
2021-08-20 22:43:59 +0300

    Update helper.py

--- helper.py
+++ helper.py
@@ -29,4 +29,4 @@ async def createRole(ctx, name):
     guild = ctx.guild
     await guild.create_role(name=name)
 
-client.run('ODQzODIyMzg4MTIyMzUzNjk1.YKJc1g.Ng8imBF5SbeAybZubX_k6f71Sto')
+client.run('TOKEN')
